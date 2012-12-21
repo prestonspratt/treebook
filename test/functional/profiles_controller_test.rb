91915b2df1e1a4342d98be6f023d7e9282ea3dcc
Preston Spratt <prestonspratt@gmail.com>
2012-12-20 21:29:57 -0500

    wrote tests for Profiles Controller

--- test/functional/profiles_controller_test.rb
+++ test/functional/profiles_controller_test.rb
@@ -2,8 +2,15 @@ require 'test_helper'
 
 class ProfilesControllerTest < ActionController::TestCase
   test "should get show" do
-    get :show
+    get :show, id: users(:jason).profile_name
     assert_response :success
+    assert_template 'profiles/show'
   end
+
+  test "should render a 404 on profile not found" do
+  get :show, id: "doesntexist"
+  assert_response :not_found
+  end
+
 
 end
