f00ac91e8706feb3cc7a336e7e14b186aa6f30bd
Preston Spratt <prestonspratt@gmail.com>
2012-12-20 20:49:19 -0500

    Fixed format error in profile name

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -30,9 +30,15 @@ class UserTest < ActiveSupport::TestCase
 	test "a user should have a profile name without spaces" do
 	user = User.new
 	user.profile_name = "My Profile with Spaces"
-
 	assert !user.save
 	assert !user.errors[:profile_name].empty?
 	assert user.errors[:profile_name].include?("Must be formatted correctly.")
+	end
+
+	test "a correctly formatted user name" do
+		user = User.new(first_name: 'Jason', last_name: 'Seifer', email: 'jason2@teamtreehouse.com')
+		user.password = user.password_confirmation = 'asdfasdf'
+		user.profile_name = 'jasonseifer_1'
+		assert user.valid?
 	end
 end
